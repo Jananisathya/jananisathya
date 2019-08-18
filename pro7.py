26d20c158ee54610a70a5122fcd8e0116ed7ff93
Jananisathya <52281059+Jananisathya@users.noreply.github.com>
2019-08-18 01:18:24 -0700

    Update pro7.py

--- pro7.py
+++ pro7.py
@@ -1,9 +1,8 @@
-#j
-chatr7=int(input())
+chatr12=int(input())
 gr7=[]
 dog7=0
-for h in range (0,chatr7+1):
-    dog7=abs((2**h)-chatr7)
+for h in range (0,chatr12+1):
+    dog7=abs((2**h)-chatr12)
     gr7.append(dog7)
 kall=min(gr7)
 print(kall)
