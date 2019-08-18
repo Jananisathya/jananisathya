152464e0f41a9a0d60e378d0e78f8eb01c48a12f
Jananisathya <52281059+Jananisathya@users.noreply.github.com>
2019-08-18 00:59:07 -0700

    Update pro4.py

--- pro4.py
+++ pro4.py
@@ -1,12 +1,12 @@
 #j
-c29,c29=map(str,input().split())
+c28,c28=map(str,input().split())
 yas=0
-if len(c19)>len(c29):
-  c19,c29=c29,c19
+if len(c18)>len(c28):
+  c18,c28=c28,c18
 p=0
-while p<len(c19):
-  yas+=(ord(c29[p])-ord(c19[p]))
+while p<len(c18):
+  yas+=(ord(c28[p])-ord(c18[p]))
   p+=1
-for p in range(p,len(c29)):
-  yas+=ord(c29[p])-ord('a')+1
+for p in range(p,len(c28)):
+  yas+=ord(c28[p])-ord('a')+1
 print(yas)
